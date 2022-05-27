edd8b6c06927066fd8658acc6f79c5ef9bbdbeac
ratamon <ratamon91@gmail.com>
2022-05-27 22:25:31 +0900

    read file

---
+++ src/main/java/projava/ReadFile.java
@@ -0,0 +1,13 @@
+package projava;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class ReadFile {
+    public static void main(String[] args) throws IOException {
+        var p = Path.of("test.txt");
+        String s = Files.readString(p);
+        System.out.println(s);
+    }
+}
