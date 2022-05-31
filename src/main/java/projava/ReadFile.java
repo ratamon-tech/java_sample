dc5520533f19a0285e2cacc3d3db6c9b51802866
ratamon <ratamon91@gmail.com>
2022-05-31 21:47:26 +0900

    try catch追加

--- src/main/java/projava/ReadFile.java
+++ src/main/java/projava/ReadFile.java
@@ -2,12 +2,17 @@ package projava;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 
 public class ReadFile {
     public static void main(String[] args) throws IOException {
-        var p = Path.of("test.txt");
-        String s = Files.readString(p);
-        System.out.println(s);
+        try {
+            var p = Path.of("hoge.txt");
+            String s = Files.readString(p);
+            System.out.println(s);
+        } catch (NoSuchFileException e) {
+            System.out.println("ファイルが見つかりません。" + e.getFile());
+        }
     }
 }
